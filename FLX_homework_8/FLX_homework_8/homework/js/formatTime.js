4c31f00afc80550ec7446ab2befa605bcb07e28e
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-02-12 16:48:50 +0200

    Delete formatTime.js

--- FLX_homework_8/FLX_homework_8/homework/js/formatTime.js
+++
@@ -1,9 +0,0 @@
-function formatTime (totalMinutes) {
-var days = Math.floor(totalMinutes / 1440);
-totalMinutes %= 1440;
-var hours = Math.floor(totalMinutes / 60);
-var minutes = totalMinutes % 60;
-console.log(days + "day(s)" + hours + "hour(s)" + minutes + "minute(s)" );
-}
-
-formatTime(2500)
